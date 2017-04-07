eb32da6d2668925426694ebd1e0e27920dd2a6aa
TSITCL\s162190 <kilito.shuto.bb@gmail.com>
2017-04-07 11:52:27 +0900

    no message

---
+++ GameJam/Classes/21/GamePlay.cpp
@@ -0,0 +1,20 @@
+
+#include "..\..\pch.h"
+#include "..\..\Game.h"
+#include "GamePlay.h"
+
+GamePlay::GamePlay()
+{
+}
+
+GamePlay::~GamePlay()
+{
+}
+
+void GamePlay::Update()
+{
+}
+
+void GamePlay::Render()
+{
+}
