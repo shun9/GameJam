2a2f8851ff4f4d5aeca6dfb1b6582c831a34e894
TSITCL\s162190 <kilito.shuto.bb@gmail.com>
2017-04-10 16:23:03 +0900

    no message

--- GameJam/Classes/21/GamePlay.h
+++ GameJam/Classes/21/GamePlay.h
@@ -8,9 +8,7 @@
 #include <vector>
 #include "GameScene.h"
 #include "..\8\Panel.h"
-
 #include "../8/MouseManager.h"
-
 #include "../20/Player.h"
 
 struct OPTION
@@ -62,12 +60,15 @@ private:
 	Microsoft::WRL::ComPtr<ID3D11Device> m_device;
 	Microsoft::WRL::ComPtr<ID3D11DeviceContext> m_context;
 
+	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> m_result;
+	//スプライト描画
+	std::unique_ptr<DirectX::SpriteBatch> m_spriteBatch;
+	DirectX::SimpleMath::Vector2 m_resultorigin;
+
 	//スコア
 	int m_score;
 
-	//ゲームオーバーフラグ
-	bool m_isGameOver;
-
+	bool m_isGameover;
 	/*--メンバ関数--*/
 public:
 	GamePlay(Microsoft::WRL::ComPtr<ID3D11Device> device
@@ -86,7 +87,7 @@ private:
 	//ゲームシステム関連
 	void FitOption();
 	void PanelSlide();
-	bool IsDead();
+	void CheckGame();
 	void GameOver();
 	
 	//選んだ選択肢を取得する
